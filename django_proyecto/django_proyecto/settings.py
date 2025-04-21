8802a0fea23a5cb3b455fc63d2abe5360f6b86e1
Ruben <ruben.sanhueza14@gmail.com>
2025-04-21 19:07:08 -0400

    Agrega soporte para despliegue en producción (DB + estáticos)

--- django_proyecto/django_proyecto/settings.py
+++ django_proyecto/django_proyecto/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -174,3 +178,18 @@ SIMPLE_JWT = {
 
 ## Para iniciar el servidor con SSL
 ##uvicorn django_proyecto.asgi:application --ssl-keyfile=key.pem --ssl-certfile=cert.pem --host localhost --port 8000 --loop asyncio
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
